f90e339dac59a57501a5504fe3783b1203257c28
agustinscuotriucel <agustinscuotricf@gmail.com>
2024-08-27 13:46:00 +0000

    ejercicio1

---
+++ Ejercicios/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class ejercicio2
+{
+    public static void Main(string[] args)
+    {
+        int numero, cifras;
+
+        Console.Write("Ingrese un numero entero y positivo: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        if(numero < 0)
+        {
+            Console.Write("Error. Debe ingresar un numero entero.");
+        }
+        else
+        {
+            cifras = 0;
+            while (numero > 0)
+            {
+                numero /= 10; 
+                cifras++;
+            }
+            Console.Write($"El numero {numero} tiene {cifras} cifras.");
+        }
+
+    }
+}
